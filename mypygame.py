282f1e43ed3af8ab1b6d246b56f0f408e0b47795
Bibek <bhurtellbbek@gmail.com>
2024-08-24 20:23:19 +0545

    snailgame

--- mypygame.py
+++ mypygame.py
@@ -7,6 +7,21 @@ def score_display():
     score_surface=test_font.render(f"{current_time}",False,(64,64,64))
     score_rect=score_surface.get_rect(center=(400,50))
     screen.blit(score_surface,score_rect)
+    return current_time
+
+def display_end_screen(final_score, high_score):
+    screen.fill((94, 129, 162))
+    final_score_surface = test_font.render(f"Your Score: {final_score}", False, (255, 255, 255))
+    final_score_rect = final_score_surface.get_rect(center=(400, 200))
+    screen.blit(final_score_surface, final_score_rect)
+
+    high_score_surface = test_font.render(f"High Score: {high_score}", False, (255, 255, 255))
+    high_score_rect = high_score_surface.get_rect(center=(400, 100))
+    screen.blit(high_score_surface, high_score_rect)
+
+    restart_surface = test_font.render("Press SPACE to start again", False, (255, 255, 255))
+    restart_rect = restart_surface.get_rect(center=(400, 300))
+    screen.blit(restart_surface, restart_rect)
 
 pygame.init()
 screen=pygame.display.set_mode((800,400))
@@ -23,6 +38,8 @@ grd_surface=pygame.image.load('graphics/ground.png').convert()
 # score_rect=score_surface.get_rect(center=(400,50))
 game_active=True
 start_time=0
+final_score=0
+high_score=0
 
 snail_surface=pygame.image.load('graphics/snail/snail1.png').convert_alpha()
 snail_rect=snail_surface.get_rect(bottomright=(600,300))
@@ -64,7 +81,7 @@ while True:
         if player_rect.bottom >=300:
             player_rect.bottom=300
         screen.blit(player_surf,(player_rect))
-        score_display()
+        final_score=score_display()
         # keys=pygame.key.get_pressed()
         # if keys[pygame.K_SPACE]
 
@@ -72,8 +89,11 @@ while True:
         #   print("collided!!!")
         if snail_rect.colliderect(player_rect):
             game_active=False
+
+            if final_score >high_score:
+                high_score=final_score
     else:
-        screen.fill('red')    
+        display_end_screen(final_score, high_score)
 
     clock.tick(60)
     
